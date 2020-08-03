145aef8c4dbda1b3f92a224edaa2cba515d890fa
yummy <476666022@qq.com>
2020-08-03 14:28:49 +0800

    :bookmark: 配置全局 scss

---
+++ vue.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    /* 开发服务相关 */
+    devServer: {
+        open: true,         //启动后浏览器自动打开页面
+        host: 'localhost',
+        port: 8088,
+    }, 
+    /* css配置相关 */
+    css: {
+        extract: true,      //在生产环境中将组件内的 css 提取到单独的 css 文件中
+        sourceMap: false,   //是否开启 CSS source map
+        modules: false,     
+        loaderOptions: {
+            sass: {
+                /* 全局引用 scss 文件 */
+                prependData: `@import "@/common/style/base.scss";`
+            }
+        },
+    }
+}
