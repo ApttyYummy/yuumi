bd08591ff44141b3fa05021b3769e893b422bbd0
yummy <476666022@qq.com>
2020-08-12 16:11:05 +0800

    :heavy_plus_sign: mockjs + axios

---
+++ src/common/mock/index.js
@@ -0,0 +1,21 @@
+import Mock from 'mockjs';
+
+/* 生成商品列表 */
+Mock.mock('/getGoodsList', {
+    "data|4": [{
+        'goodsId|+1': 1001,
+        'name': '@first',
+        'price|10-20': 10,
+        'checked': false,
+    }],
+});
+
+/* 生成优惠券列表 */
+Mock.mock('/getCouponList', {
+    "data|5": [{
+        'name': '@name',
+        'amount|1-10': 1,
+        'couponId|1000-9999': 1000,
+        'checked': false,
+    }],
+});
